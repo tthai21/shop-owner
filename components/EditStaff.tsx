23e11e4a1f716d143418dd6a1c933a99eb1a2415
Alex Thai <tthai21@student.holmesglen.edu.au>
2024-06-01 18:40:34 +1000

    Staff card

--- components/EditStaff.tsx
+++ components/EditStaff.tsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import * as Dialog from "@radix-ui/react-dialog";
 import { Cross2Icon } from "@radix-ui/react-icons";
 import SwitchActive from "./SwitchActive";
-
+import axios from "@/ulti/axios";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 
 interface EditStaffProps {
@@ -21,18 +22,44 @@ const EditStaff: React.FC<EditStaffProps> = ({ staff }) => {
     setFormData({ ...formData, active });
   };
 
-  const handleSubmit = () => {
-    // Handle form submission, e.g., send data to API
-    console.log(formData);
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("/staff/", formData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log(response.data);
+
+      if (response.status !== 201) {
+        throw new Error("Failed to submit booking.");
+      }
+    } catch (error) {
+      console.error("Error submitting booking:", error);
+    }
   };
-console.log(formData);
 
   return (
     <Dialog.Root>
       <Dialog.Trigger asChild>
-        <button className="bg-blue-500 border border-blue-500 w-[80px] inline-flex h-[35px] items-center justify-center text-white px-[15px] font-medium leading-none rounded-md">
-          Edit
-        </button>
+      <div
+        key={staff.id}
+        className="lg:p-4 mb-10 bg-white border-2 rounded-lg shadow-md py-2 mx-5 flex flex-col justify-center items-center w-[200px]"
+      >
+
+        <div className="text-base xs:text-sm font-semibold flex flex-col justify-center gap-2 mb-2 items-center">
+          <AccountCircleIcon />
+          {staff.firstName}
+        </div>
+        <div className="text-gray-600 mb-4 text-base flex justify-center flex-col-1 gap-x-2">
+          <div>Nickname: </div>
+          <div>{staff.nickName}</div>
+        </div>        
+        {/* <DeleteStaff/> */}
+        {/* <EditStaff staff={staff}/> */}
+       
+      </div>
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="bg-black opacity-50 data-[state=open]:animate-overlayShow fixed inset-0" />
@@ -43,62 +70,64 @@ console.log(formData);
           <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
             Make changes to staff profile here. Click save when you're done.
           </Dialog.Description>
-          <StaffField
-            htmlFor="firstName"
-            fieldName="First Name"
-            value={formData.firstName}
-            onChange={handleChange}
-          />
-          <StaffField
-            htmlFor="lastName"
-            fieldName="Last Name"
-            value={formData.lastName}
-            onChange={handleChange}
-          />
-          <StaffField
-            htmlFor="nickName"
-            fieldName="Nick Name"
-            value={formData.nickName}
-            onChange={handleChange}
-          />
-          <StaffField
-            htmlFor="phone"
-            fieldName="Phone Number"
-            value={formData.phone}
-            onChange={handleChange}
-          />
-          <StaffField
-            htmlFor="skillLevel"
-            fieldName="Skill Level"
-            value={formData.skillLevel}
-            onChange={handleChange}
-          />
-          <StaffField
-            htmlFor="dateOfBirth"
-            fieldName="Date Of Birth"
-            value={formData.dateOfBirth}
-            onChange={handleChange}
-          />
-          <StaffField
-            htmlFor="workingDays"
-            fieldName="Working Days"
-            value={formData.workingDays}
-            onChange={handleChange}
-          />
-          <SwitchActive
-            active={formData.active}
-            onChange={handleSwitchChange}
-          />
-          <div className="mt-[25px] flex justify-end">
-            <Dialog.Close asChild>
-              <button
-                onClick={handleSubmit}
-                className="bg-green4 text-green11 hover:bg-green5 focus:shadow-green7 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none"
-              >
-                Save changes
-              </button>
-            </Dialog.Close>
-          </div>
+          <form action="submit" onSubmit={handleSubmit}>
+            <StaffField
+              htmlFor="firstName"
+              fieldName="First Name"
+              value={formData.firstName}
+              onChange={handleChange}
+            />
+            <StaffField
+              htmlFor="lastName"
+              fieldName="Last Name"
+              value={formData.lastName}
+              onChange={handleChange}
+            />
+            <StaffField
+              htmlFor="nickName"
+              fieldName="Nick Name"
+              value={formData.nickName}
+              onChange={handleChange}
+            />
+            <StaffField
+              htmlFor="phone"
+              fieldName="Phone Number"
+              value={formData.phone}
+              onChange={handleChange}
+            />
+            <StaffField
+              htmlFor="skillLevel"
+              fieldName="Skill Level"
+              value={formData.skillLevel}
+              onChange={handleChange}
+            />
+            <StaffField
+              htmlFor="dateOfBirth"
+              fieldName="Date Of Birth"
+              value={formData.dateOfBirth}
+              onChange={handleChange}
+            />
+            <StaffField
+              htmlFor="workingDays"
+              fieldName="Working Days"
+              value={formData.workingDays}
+              onChange={handleChange}
+            />
+            <SwitchActive
+              active={formData.active}
+              onChange={handleSwitchChange}
+            />
+            <div className="mt-[25px] flex justify-end">
+              <Dialog.Close asChild>
+                <button
+                  type="submit"
+                  className="bg-blue-700 text-white hover:bg-green5 focus:shadow-green7 inline-flex h-[35px] items-center justify-center rounded-md px-[15px] font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none"
+                >
+                  Save changes
+                </button>
+              </Dialog.Close>
+            </div>
+          </form>
           <Dialog.Close asChild>
             <button
               className="text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
@@ -122,7 +151,12 @@ interface StaffFieldProps {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const StaffField: React.FC<StaffFieldProps> = ({ value, fieldName, htmlFor, onChange }) => {
+const StaffField: React.FC<StaffFieldProps> = ({
+  value,
+  fieldName,
+  htmlFor,
+  onChange,
+}) => {
   return (
     <fieldset className="mb-[15px] flex items-center gap-5">
       <label
--- components/Staff.tsx
+++ components/Staff.tsx
@@ -11,21 +11,21 @@ const Staff: React.FC<StaffProps> = ({ staff }) => {
     return (
         <div
         key={staff.id}
-        className="lg:p-4 mb-10 bg-white border-2 rounded-lg shadow-md py-2 mx-5 flex flex-col justify-center items-center w-[200px]"
+        className='cursor-pointer'
+        // className="lg:p-4 mb-10 bg-white border-2 rounded-lg shadow-md py-2 mx-5 flex flex-col justify-center items-center w-[200px]"
       >
 
-        <div className="text-base xs:text-sm font-semibold flex flex-col justify-center gap-2 mb-2 items-center">
+        {/* <div className="text-base xs:text-sm font-semibold flex flex-col justify-center gap-2 mb-2 items-center">
           <AccountCircleIcon />
           {staff.firstName}
         </div>
         <div className="text-gray-600 mb-4 text-base flex justify-center flex-col-1 gap-x-2">
           <div>Nickname: </div>
           <div>{staff.nickName}</div>
-        </div>
-        <div className='flex gap-3'>
-        <DeleteStaff/>
+        </div>         */}
+        {/* <DeleteStaff/> */}
         <EditStaff staff={staff}/>
-        </div>
+       
       </div>
     );
 };
