29fd81037bb11eb9d90f129314a61b4565262a4b
Alex Thai <tthai21@student.holmesglen.edu.au>
2024-06-20 20:11:17 +1000

    test

--- components/Menubar.tsx
+++ components/Menubar.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import * as Menubar from "@radix-ui/react-menubar";
 import { useRouter } from "next/router";
+
 interface MenuItem {
   label: string;
   path: string;
   onClick?: () => void;
 }
+
 const MenubarDemo = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const toggleMenu = () => {
@@ -13,6 +15,7 @@ const MenubarDemo = () => {
   };
   const router = useRouter();
   const slug = router.route;
+
   const logoutHandler = () => {
     router.push("/");
     sessionStorage.removeItem("authToken");
@@ -26,28 +29,27 @@ const MenubarDemo = () => {
     { label: "Bookings", path: "/bookings" },
     { label: "Logout", path: "", onClick: logoutHandler },
   ];
+
   return (
-    <div className="mb-[100px]">
-      <div className="flex justify-between items-center relative font-bold text-lg z-10 ">
+    <div className="mb-[100px] relative  z-10">
+      <div className="flex justify-between items-center font-bold text-lg">
         <div
-          className={` mx-5 md:hidden ${
+          className={`absolute top-[60px] left-0 right-0 bg-white shadow-md rounded-md md:hidden ${
             isOpen ? "block" : "hidden"
-          } absolute top-[60px] left-0 right-0 bg-white shadow-md rounded-md`}
+          }`}
         >
           {menuItems.map((menuItem, index) => (
             <div key={index}>
               <div
                 onClick={() => {
-                  {
-                    menuItem.label === "Logout"
-                      ? logoutHandler()
-                      : router.push(menuItem.path);
-                  }
+                  menuItem.label === "Logout"
+                    ? logoutHandler()
+                    : router.push(menuItem.path);
                   setIsOpen(false);
                 }}
-                className={`py-2 px-3 outline-none select-none font-bold leading-none rounded text-slate-900 text-[15px] lg:text-base flex items-center justify-between gap-[4px]   hover:underline  ${
+                className={`py-2 px-3 outline-none select-none font-bold leading-none rounded text-slate-900 text-[15px] lg:text-base flex items-center justify-between gap-[4px] hover:underline ${
                   slug == menuItem.path && "underline underline-offset-4"
-                } `}
+                }`}
               >
                 {menuItem.label}
               </div>
@@ -81,69 +83,24 @@ const MenubarDemo = () => {
           </div>
         </div>
       </div>
-      {/* laptop */}
-      <Menubar.Root className="hidden md:flex bg-white p-[3px] mt-5 w-[90%] sx:w-[70%] lg:w-[50%] mx-auto justify-center  rounded-md shadow-[0_2px_10px] shadow-blackA4">
-        <Menubar.Menu>
-          <Menubar.Trigger
-            onClick={() => router.push("/dashboard")}
-            className={`py-2 px-3 outline-none select-none font-medium leading-none rounded text-slate-900 text-[13px] lg:text-base flex items-center justify-between gap-[2px] data-[highlighted]:bg-violet4 data-[state=open]:bg-violet4 hover:underline hover:underline-offset-4 ${
-              slug == "/dashboard" && "underline underline-offset-4"
-            } `}
-          >
-            Dashboard
-          </Menubar.Trigger>
-        </Menubar.Menu>
-        <Menubar.Menu>
-          <Menubar.Trigger
-            onClick={() => router.push("/staffs")}
-            className={`py-2 px-3 outline-none select-none font-medium leading-none rounded text-slate-900 text-[13px] lg:text-base flex items-center justify-between gap-[2px] data-[highlighted]:bg-violet4 data-[state=open]:bg-violet4 hover:underline hover:underline-offset-4 ${
-              slug == "/staffs" && "underline underline-offset-4"
-            } `}
-          >
-            Staffs
-          </Menubar.Trigger>
-        </Menubar.Menu>
-
-        <Menubar.Menu>
-          <Menubar.Trigger
-            onClick={() => router.push("/services")}
-            className={`py-2 px-3 outline-none select-none font-medium leading-none rounded text-slate-900 text-[13px] lg:text-base flex items-center justify-between gap-[2px] data-[highlighted]:bg-violet4 data-[state=open]:bg-violet4 hover:underline hover:underline-offset-4 ${
-              slug == "/services" && "underline underline-offset-4"
-            } `}
-          >
-            Services
-          </Menubar.Trigger>
-        </Menubar.Menu>
-
-        <Menubar.Menu>
-          <Menubar.Trigger
-            onClick={() => router.push("/bookings")}
-            className={`py-2 px-3 outline-none select-none font-medium leading-none rounded text-slate-900 text-[13px] lg:text-base flex items-center justify-between gap-[2px] data-[highlighted]:bg-violet4 data-[state=open]:bg-violet4 hover:underline hover:underline-offset-4 ${
-              slug == "/bookings" && "underline underline-offset-4"
-            } `}
-          >
-            Bookings
-          </Menubar.Trigger>
-        </Menubar.Menu>
-
-        {/* <Menubar.Menu>
-          <Menubar.Trigger
-            onClick={() => router.push("/profiles")}
-            className={`py-2 px-3 outline-none select-none font-medium leading-none rounded text-slate-900 text-[13px] lg:text-base flex items-center justify-between gap-[2px] data-[highlighted]:bg-violet4 data-[state=open]:bg-violet4 hover:underline hover:underline-offset-4 ${
-              slug == "/profiles" && "underline underline-offset-4"
-            } `}
-          >
-            Profiles
-          </Menubar.Trigger>
-        </Menubar.Menu> */}
-        <Menubar.Menu>
-          <Menubar.Trigger
-            onClick={logoutHandler}
-            className={`py-2 px-3 outline-none select-none font-medium leading-none rounded text-slate-900 text-[13px] lg:text-base flex items-center justify-between gap-[2px] data-[highlighted]:bg-violet4 data-[state=open]:bg-violet4 hover:underline hover:underline-offset-4 `}
-          >
-            Logout
-          </Menubar.Trigger>
-        </Menubar.Menu>
+      {/* Laptop and larger screens */}
+      <Menubar.Root className="hidden md:flex bg-white p-[3px] mt-5 w-[90%] sm:w-[70%] lg:w-[50%] mx-auto justify-center rounded-md shadow-[0_2px_10px] shadow-blackA4">
+        {menuItems.map((menuItem, index) => (
+          <Menubar.Menu key={index}>
+            <Menubar.Trigger
+              onClick={() =>
+                menuItem.label === "Logout"
+                  ? logoutHandler()
+                  : router.push(menuItem.path)
+              }
+              className={`py-2 px-3 outline-none select-none font-medium leading-none rounded text-slate-900 text-[13px] lg:text-base flex items-center justify-between gap-[2px] hover:underline hover:underline-offset-4 ${
+                slug == menuItem.path && "underline underline-offset-4"
+              }`}
+            >
+              {menuItem.label}
+            </Menubar.Trigger>
+          </Menubar.Menu>
+        ))}
       </Menubar.Root>
     </div>
   );
